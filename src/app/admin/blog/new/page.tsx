cb117fd35566ca7ec2e4faee39e02a3ea3a0eb3e
zoomnearbynetwork <zoomnearbynetworks@gmail.com>
2026-06-30 12:45:03 +0530

    fix: blog editor missing MDEditor stylesheets causing unstyled white overflow

--- src/app/admin/blog/new/page.tsx
+++ src/app/admin/blog/new/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import '@uiw/react-md-editor/markdown-editor.css';
+import '@uiw/react-markdown-preview/markdown.css';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useTheme } from '@/components/ThemeProvider';
